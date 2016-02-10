15a0a9b5cd5bc3b7c944491168bcce976977fcf5
Victor <theonlypwner@users.noreply.github.com>
2016-02-09 19:09:53 -0700

    Fix member names in ServerList

--- source/src/client/gui/ServerList.cpp
+++ source/src/client/gui/ServerList.cpp
@@ -3,11 +3,11 @@
 
 ServerList::ServerList(wxWindow *parent, wxWindowID id) : wxPanel(parent, id)
 {
-    m_List = new wxListCtrl(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLC_REPORT);
-    m_List->AppendColumn("Server");
-    m_List->AppendColumn("Ping");
+    m_pList = new wxListCtrl(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLC_REPORT);
+    m_pList->AppendColumn("Server");
+    m_pList->AppendColumn("Ping");
 
-    m_Sizer = new wxBoxSizer(wxHORIZONTAL);
-    m_Sizer->Add(m_List, 1, wxEXPAND | wxALL);
-    SetSizer(m_Sizer);
+    m_pSizer = new wxBoxSizer(wxHORIZONTAL);
+    m_pSizer->Add(m_pList, 1, wxEXPAND | wxALL);
+    SetSizer(m_pSizer);
 }
